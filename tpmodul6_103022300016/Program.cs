58ea0eae548f66c5c3ec00f5f88ae4f8ca015c0d
edselspth <edselspth@gmail.com>
2025-03-22 14:02:06 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_103022300016/Program.cs
+++ tpmodul6_103022300016/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace tpmodul6_103022300016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design by Contract - Edsel Septa Haryanto");
+            video.printVideoDetail();
+            Console.WriteLine();
+            video.increasePlayCount(1000000000);
+            video.printVideoDetail();
+        }
+    }
+}
---
+++ tpmodul6_103022300016/SayaTubeVideo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_103022300016
+{
+    class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+        Random random = new Random();
+
+        public SayaTubeVideo(String title)
+        // Constructor meminta input title dan menginisialisasi id dan playCount
+        {
+            this.id = random.Next(10000, 99999); // id diinisialisasi dengan random number
+            this.title = title; 
+            this.playCount = 0; // play count awal di set 0
+        }
+
+        public void increasePlayCount(int views)
+        // Method untuk menambah jumlah view
+        {
+            this.playCount += views;
+        }
+
+        public void printVideoDetail()
+        // Method untuk menampilkan detail video
+        {
+            Console.WriteLine("ID Video: " + this.id);
+            Console.WriteLine("Judul Video: " + this.title);
+            Console.WriteLine("Jumlah View: " + this.playCount);
+        }
+    }
+}
