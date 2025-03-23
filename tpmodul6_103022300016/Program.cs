41f5cc80f222bb423d028a0fdd29daf7a1fbf2e9
edselspth <edselspth@gmail.com>
2025-03-23 09:02:18 +0700

    implementasi design by contract

--- tpmodul6_103022300016/Program.cs
+++ tpmodul6_103022300016/Program.cs
@@ -5,11 +5,31 @@ namespace tpmodul6_103022300016
     {
         static void Main(string[] args)
         {
-            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design by Contract - Edsel Septa Haryanto");
-            video.printVideoDetail();
-            Console.WriteLine();
-            video.increasePlayCount(1000000000);
-            video.printVideoDetail();
+            try
+            {
+                SayaTubeVideo video = new SayaTubeVideo("Tutorial Design by Contract - Edsel Septa Haryanto");
+                video.printVideoDetail();
+                Console.WriteLine();
+                for (int i = 0; i < 1000; i++)
+                //Perulangan untuk menambahkan jumlah view sebanyak 1000 kali
+                {
+                    video.increasePlayCount(10000000);
+                    video.printVideoDetail();
+                    Console.WriteLine();
+
+                    if(video.getPlayCount() == int.MaxValue)
+                    // untuk menghentikan perulangan ketika jumlah view mencapai batas maksimum
+                    {
+                        break;
+                    }
+                }
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error!!! " + e.Message);
+            }
+            
         }
     }
 }
--- tpmodul6_103022300016/SayaTubeVideo.cs
+++ tpmodul6_103022300016/SayaTubeVideo.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,21 +13,41 @@ namespace tpmodul6_103022300016
         private int id;
         private string title;
         private int playCount;
-        Random random = new Random();
+        Random random = new Random(); // random untuk menambahkan id video
 
         public SayaTubeVideo(String title)
         // Constructor meminta input title dan menginisialisasi id dan playCount
         {
+            Debug.Assert(title != null, "Judul tidak boleh kosong");
+            Debug.Assert(title.Length <= 100, "Judul tidak boleh lebih dari 100 kata");
             this.id = random.Next(10000, 99999); // id diinisialisasi dengan random number
             this.title = title; 
             this.playCount = 0; // play count awal di set 0
         }
 
         public void increasePlayCount(int views)
-        // Method untuk menambah jumlah view
         {
-            this.playCount += views;
+            // Pre-condition: Memastikan nilai views dalam rentang yang benar
+            Debug.Assert(views <= 10000000, "Jumlah view harus antara 1 hingga 10.000.000"); 
+
+            try
+            {
+                checked  // Memastikan jika terjadi overflow, akan dilempar exception
+                {   
+                    if (this.playCount > int.MaxValue - views)
+                    {
+                        throw new OverflowException(); // Melempar exception jika playcount melebihi batas maksimum int
+                    }
+                        this.playCount += views; // Menambahkan views ke playCount
+                }
+            }
+            catch (OverflowException) // exception untuk menghentikan jika penjumlah playcount melebih batas maksimum int
+            {
+                Console.WriteLine("Error!!! Sudah Mencapai Batas Maksimum Views.");
+                this.playCount = int.MaxValue; // Set playCount ke batas maksimal agar tidak melebihi batas dan tetap jumlah view sampai ke max value int
+            }
         }
+
 
         public void printVideoDetail()
         // Method untuk menampilkan detail video
@@ -34,5 +56,12 @@ namespace tpmodul6_103022300016
             Console.WriteLine("Judul Video: " + this.title);
             Console.WriteLine("Jumlah View: " + this.playCount);
         }
+
+        public int getPlayCount()
+        // getter untuk menadapatkan nilai playcount
+        {
+            return this.playCount;
+        }
+
     }
 }
